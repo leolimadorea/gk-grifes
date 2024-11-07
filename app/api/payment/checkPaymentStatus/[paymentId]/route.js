87fd070addd4293213bb71609a04acfd237a3e07
Leonardo <leolimadorea@gmail.com>
2024-11-07 19:19:46 -0300

    bildou

--- app/api/payment/checkPaymentStatus/[paymentId]/route.js
+++ app/api/payment/checkPaymentStatus/[paymentId]/route.js
@@ -1,10 +1,12 @@
 import { checkPaymentStatus } from "@/app/db/payment/checkPaymentStatus";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }) {
+export async function GET(_, { params }) {
+  // Substitua `request` por `_` para indicar que ele não é utilizado
   try {
     const { paymentId } = params;
     console.log("CHECK PAYMENT STATUS", paymentId);
+
     const payment = await checkPaymentStatus(paymentId);
 
     return NextResponse.json(payment, { status: 200 });
