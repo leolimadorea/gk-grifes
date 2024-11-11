734669f65f8d9265988baa826bf15b295d64251a
Gustavo Paçó <gustavopacosa@gmail.com>
2024-11-11 14:37:27 -0300

    aloba

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,46 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import "../public/scss/main.scss";
 import "photoswipe/dist/photoswipe.css";
 import "rc-slider/assets/index.css";
-import { useEffect, useState } from "react";
-import Context from "../context/Context.jsx";
-import "../public/scss/main.scss";
-import NextAuthSessionProvider from "./SessionProvider";
-
+import HomesModal from "@/components/modals/HomesModal";
+import Context from "@/context/Context";
+import QuickView from "@/components/modals/QuickView";
+import ProductSidebar from "@/components/modals/ProductSidebar";
+import QuickAdd from "@/components/modals/QuickAdd";
+import Compare from "@/components/modals/Compare";
+import ShopCart from "@/components/modals/ShopCart";
 import AskQuestion from "@/components/modals/AskQuestion";
 import BlogSidebar from "@/components/modals/BlogSidebar";
 import ColorCompare from "@/components/modals/ColorCompare";
-import Compare from "@/components/modals/Compare";
 import DeliveryReturn from "@/components/modals/DeliveryReturn";
 import FindSize from "@/components/modals/FindSize";
 import Login from "@/components/modals/Login";
 import MobileMenu from "@/components/modals/MobileMenu";
-import ProductSidebar from "@/components/modals/ProductSidebar";
-import QuickAdd from "@/components/modals/QuickAdd";
-import QuickView from "@/components/modals/QuickView";
 import Register from "@/components/modals/Register";
 import ResetPass from "@/components/modals/ResetPass";
 import SearchModal from "@/components/modals/SearchModal";
-import ShopCart from "@/components/modals/ShopCart";
 import ToolbarBottom from "@/components/modals/ToolbarBottom";
 import ToolbarShop from "@/components/modals/ToolbarShop";
 
-import ScrollTop from "@/components/common/ScrollTop";
-import ShareModal from "@/components/modals/ShareModal";
 import { usePathname } from "next/navigation";
+import NewsletterModal from "@/components/modals/NewsletterModal";
+import ShareModal from "@/components/modals/ShareModal";
+import ScrollTop from "@/components/common/ScrollTop";
+import RtlToggle from "@/components/common/RtlToggle";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-
   useEffect(() => {
     if (typeof window !== "undefined") {
-      import("bootstrap/dist/js/bootstrap.esm").then(() => {});
+      // Import the script only on the client side
+      import("bootstrap/dist/js/bootstrap.esm").then(() => {
+        // Module is imported, you can access any exported functionality if
+      });
     }
   }, []);
-
   useEffect(() => {
     const handleScroll = () => {
       const header = document.querySelector("header");
@@ -50,105 +53,104 @@ export default function RootLayout({ children }) {
 
     window.addEventListener("scroll", handleScroll);
 
-    // Função de limpeza para remover o event listener quando o componente for desmontado
+    // Cleanup function to remove event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, []); // Empty dependency array means this effect runs once on mount and cleans up on unmount
 
   const [scrollDirection, setScrollDirection] = useState("down");
 
   useEffect(() => {
     setScrollDirection("up");
-    const lastScrollY = { current: window.scrollY };
-
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
       if (currentScrollY > 250) {
         if (currentScrollY > lastScrollY.current) {
-          // Rolando para baixo
+          // Scrolling down
           setScrollDirection("down");
         } else {
-          // Rolando para cima
+          // Scrolling up
           setScrollDirection("up");
         }
       } else {
-        // Abaixo de 250px
+        // Below 250px
         setScrollDirection("down");
       }
 
       lastScrollY.current = currentScrollY;
     };
 
-    // Adiciona o event listener de scroll
+    const lastScrollY = { current: window.scrollY };
+
+    // Add scroll event listener
     window.addEventListener("scroll", handleScroll);
 
-    // Função de limpeza: remove o event listener quando o componente for desmontado
+    // Cleanup: remove event listener when component unmounts
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [pathname]);
+  useEffect(() => {
+    // Close any open modal
+    const bootstrap = require("bootstrap"); // dynamically import bootstrap
+    const modalElements = document.querySelectorAll(".modal.show");
+    modalElements.forEach((modal) => {
+      const modalInstance = bootstrap.Modal.getInstance(modal);
+      if (modalInstance) {
+        modalInstance.hide();
+      }
+    });
+
+    // Close any open offcanvas
+    const offcanvasElements = document.querySelectorAll(".offcanvas.show");
+    offcanvasElements.forEach((offcanvas) => {
+      const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
+      if (offcanvasInstance) {
+        offcanvasInstance.hide();
+      }
+    });
+  }, [pathname]); // Runs every time the route changes
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      import("bootstrap").then((bootstrap) => {
-        const modalElements = document.querySelectorAll(".modal.show");
-        modalElements.forEach((modal) => {
-          const modalInstance = bootstrap.Modal.getInstance(modal);
-          if (modalInstance) modalInstance.hide();
-        });
-
-        const offcanvasElements = document.querySelectorAll(".offcanvas.show");
-        offcanvasElements.forEach((offcanvas) => {
-          const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
-          if (offcanvasInstance) offcanvasInstance.hide();
-        });
-      });
-    }
-  }, [pathname]);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const header = document.querySelector("header");
-      if (header) {
-        if (scrollDirection === "up") {
-          header.style.top = "0px";
-        } else {
-          header.style.top = "-185px";
-        }
+    const header = document.querySelector("header");
+    if (header) {
+      if (scrollDirection == "up") {
+        header.style.top = "0px";
+      } else {
+        header.style.top = "-185px";
       }
     }
   }, [scrollDirection]);
-
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      import("wowjs").then(({ WOW }) => {
-        const wow = new WOW({
-          mobile: false,
-          live: false,
-        });
-        wow.init();
-      });
-    }
+    const { WOW } = require("wowjs");
+    const wow = new WOW({
+      mobile: false,
+      live: false,
+    });
+    wow.init();
   }, [pathname]);
-
   const [showChild, setShowChild] = useState(false);
-
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const direction = localStorage.getItem("direction")
-        ? JSON.parse(localStorage.getItem("direction")).dir
-        : "ltr";
-      document.documentElement.dir = direction;
-      document.body.classList.add(direction);
-      setTimeout(() => {
-        setShowChild(true);
-        document.getElementById("preloader").classList.add("disabled");
-      }, 800);
+    if (localStorage.getItem("direction")) {
+      // document.documentElement.dir = JSON.parse(
+      //   localStorage.getItem("direction")?.dir
+      // );
+      document.documentElement.dir = JSON.parse(
+        localStorage.getItem("direction")
+      ).dir;
+      document.body.classList.add(
+        JSON.parse(localStorage.getItem("direction")).dir
+      );
+    } else {
+      document.documentElement.dir = "ltr";
     }
-  }, []);
-
+    setTimeout(() => {
+      setShowChild(true);
+      document.getElementById("preloader").classList.add("disabled");
+    }, 800);
+  });
   return (
     <html lang="en">
       <body className="preload-wrapper">
@@ -158,10 +160,11 @@ export default function RootLayout({ children }) {
           </div>
         </div>
         <Context>
-          {showChild && (
-            <NextAuthSessionProvider>
+          {showChild ? (
+            <>
               <div id="wrapper">{children}</div>
-              <QuickView />
+              <RtlToggle />
+              <HomesModal /> <QuickView />
               <QuickAdd />
               <ProductSidebar />
               <Compare />
@@ -178,8 +181,11 @@ export default function RootLayout({ children }) {
               <SearchModal />
               <ToolbarBottom />
               <ToolbarShop />
-              <ShareModal />
-            </NextAuthSessionProvider>
+              <NewsletterModal />
+              <ShareModal />{" "}
+            </>
+          ) : (
+            " "
           )}
         </Context>
         <ScrollTop />
--- components/modals/Login.jsx
+++ components/modals/Login.jsx
@@ -1,136 +1,86 @@
 "use client";
-import { signIn } from "next-auth/react";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-// Importação dinâmica do Bootstrap Modal
-const Modal = dynamic(() => import("bootstrap").then((mod) => mod.Modal), {
-  ssr: false,
-});
+import React from "react";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter();
-  const modalRef = useRef(null);
-  const [isModalLoaded, setIsModalLoaded] = useState(false);
-
-  useEffect(() => {
-    // Marca o modal como carregado quando o Bootstrap Modal está disponível
-    if (Modal) setIsModalLoaded(true);
-  }, []);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setError("");
-
-    const result = await signIn("credentials", {
-      redirect: false,
-      email,
-      password,
-    });
-
-    if (result.error) {
-      setError("Credenciais inválidas. Tente novamente.");
-    } else {
-      // Exibe o toast de sucesso
-      toast.success("Login bem-sucedido!");
-
-      // Fecha o modal
-      if (isModalLoaded && modalRef.current) {
-        const modal =
-          Modal.getInstance(modalRef.current) || new Modal(modalRef.current);
-        modal.hide();
-      }
-
-      // Redireciona para a página inicial
-      router.push("/");
-    }
-  };
-
   return (
-    <>
-      <ToastContainer />
-
-      <div
-        className="modal modalCentered fade form-sign-in modal-part-content"
-        id="login"
-        ref={modalRef}
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="header">
-              <div className="demo-title">Logar</div>
-              <span
-                className="icon-close icon-close-popup"
-                data-bs-dismiss="modal"
-              />
-            </div>
-            <div className="tf-login-form">
-              <form onSubmit={handleLogin} acceptCharset="utf-8">
-                <div className="tf-field style-1">
-                  <input
-                    className="tf-field-input tf-input"
-                    placeholder=" "
-                    type="email"
-                    required
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <label className="tf-field-label">Email *</label>
-                </div>
-                <div className="tf-field style-1">
-                  <input
-                    className="tf-field-input tf-input"
-                    placeholder=" "
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <label className="tf-field-label">Senha *</label>
-                </div>
-                {error && <p style={{ color: "red" }}>{error}</p>}
-                <div>
-                  <a
-                    href="#forgotPassword"
-                    data-bs-toggle="modal"
-                    className="btn-link link"
+    <div
+      className="modal modalCentered fade form-sign-in modal-part-content"
+      id="login"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="header">
+            <div className="demo-title">Log in</div>
+            <span
+              className="icon-close icon-close-popup"
+              data-bs-dismiss="modal"
+            />
+          </div>
+          <div className="tf-login-form">
+            <form
+              onSubmit={(e) => e.preventDefault()}
+              className=""
+              acceptCharset="utf-8"
+            >
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="email"
+                  name=""
+                  required
+                  autoComplete="abc@xyz.com"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  Email *
+                </label>
+              </div>
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="password"
+                  name=""
+                  required
+                  autoComplete="current-password"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  Password *
+                </label>
+              </div>
+              <div>
+                <a
+                  href="#forgotPassword"
+                  data-bs-toggle="modal"
+                  className="btn-link link"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <div className="bottom">
+                <div className="w-100">
+                  <button
+                    type="submit"
+                    className="tf-btn btn-fill animate-hover-btn radius-3 w-100 justify-content-center"
                   >
-                    Esqueceu a Senha?
+                    <span>Log in</span>
+                  </button>
+                </div>
+                <div className="w-100">
+                  <a
+                    href="#register"
+                    data-bs-toggle="modal"
+                    className="btn-link fw-6 w-100 link"
+                  >
+                    New customer? Create your account
+                    <i className="icon icon-arrow1-top-left" />
                   </a>
                 </div>
-                <div className="bottom">
-                  <div className="w-100">
-                    <button
-                      type="submit"
-                      className="tf-btn btn-fill animate-hover-btn radius-3 w-100 justify-content-center"
-                    >
-                      <span>Entrar</span>
-                    </button>
-                  </div>
-                  <div className="w-100">
-                    <a
-                      href="#register"
-                      data-bs-toggle="modal"
-                      className="btn-link fw-6 w-100 link"
-                    >
-                      Novo por aqui? Cadastre-se
-                      <i className="icon icon-arrow1-top-left" />
-                    </a>
-                  </div>
-                </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
