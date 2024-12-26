f6348cc18eb41e6f763a51834f4bbdef963e50cf
DiogoDieger <diogaodieger@gmail.com>
2024-12-26 04:05:13 -0300

    dinamic url back

--- app/page.jsx
+++ app/page.jsx
@@ -23,9 +23,12 @@ export const revalidate = 0;
 
 export default async function Page() {
   // Fetching products
-  const res = await fetch(`http://localhost:3000/api/products`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Erro ao buscar os produtos");
