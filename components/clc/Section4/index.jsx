6eb067a8a1b08e0777afd9fe17ee9e67ea30a7e1
Leonardo Lima Dorea <leolimadorea@gmail.com>
2025-03-25 03:30:59 -0300

    new banner

--- components/clc/Section4/index.jsx
+++ components/clc/Section4/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export default function Section4() {
   return (
     <div className={styles.container}>
-      <img src="/images/home1.png" alt="Banner" />
+      <img src="/images/banner/nikebolha.png" alt="Banner" />
     </div>
   );
 }
